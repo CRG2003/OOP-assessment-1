2b2341d0d7f5c514953f085efdf2620f39771171
CRG2003 <jakecraig50@gmail.com>
2022-03-21 20:19:30 +0000

    Finished input code
    Input for txt files now works with exception handling and added exception handling to manual input

--- CMP1903M Assessment 1 Base Code/Input.cs
+++ CMP1903M Assessment 1 Base Code/Input.cs
@@ -10,29 +10,58 @@ namespace CMP1903M_Assessment_1_Base_Code
     {
         //Handles the text input for Assessment 1
         string text = "nothing";
-        
-        //Method: manualTextInput
-        //Arguments: none
-        //Returns: string
+        bool valid = false;
+
+
         //Gets text input from the keyboard
         public string manualTextInput()
         {
-            Console.Write("Enter the text: ");
-            text = Console.ReadLine();
+            // Loops while the text doesnt end in a *
+            while (valid == false){
+
+                // user input
+                Console.Write("Enter the text: ");
+                text = Console.ReadLine();
+
+                // checks for *
+                foreach (var i in text){
+                    if (i.ToString() == "*"){
+                        valid = true;
+                    }
+                }
+            }
             return text;
         }
 
-        //Method: fileTextInput
-        //Arguments: string (the file path)
-        //Returns: string
+
         //Gets text input from a .txt file
         public string fileTextInput(string fileName)
         {
+            // searches for the file given the partial name (must be .txt file)
             fileName = fileName + ".txt";
-            //text = File.ReadAllText();
-            using (var sr = new StreamReader(fileName)){
-                text = sr.ToString();
+            string folderPath = @"C:\";
+            DirectoryInfo dir= new DirectoryInfo(folderPath);
+            FileInfo[] files = dir.GetFiles(fileName, SearchOption.TopDirectoryOnly);
+
+            // loops for any found file (can only be 1) 
+            // foreach loop is used as the above code returns a list of files 
+            foreach (var item in files){
+                using (var sr = new StreamReader(item.FullName)){
+                    if (File.Exists(item.FullName)){
+                        text = sr.ReadToEnd().ToString();
+                    }
+                }
             }
+
+            // if the text is the default text (meaning no input) code is re-ran
+            if (text == "nothing"){
+                Console.WriteLine("File does not exist");
+                Console.WriteLine("");
+                Console.Write("Enter the files name: ");
+                string file = Console.ReadLine();
+                text = fileTextInput(file);
+            }
+
             return text;
         }
 
